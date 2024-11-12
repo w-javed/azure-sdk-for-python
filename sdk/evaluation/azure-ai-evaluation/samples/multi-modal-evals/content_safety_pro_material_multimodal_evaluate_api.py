ef7095040504c8f3e23a3e989581aac3ce3ba12c
Waqas Javed <7674577+w-javed@users.noreply.github.com>
2024-11-11 21:47:58 -0800

    cs_pm_run

--- sdk/evaluation/azure-ai-evaluation/samples/multi-modal-evals/content_safety_pro_material_multimodal_evaluate_api.py
+++ sdk/evaluation/azure-ai-evaluation/samples/multi-modal-evals/content_safety_pro_material_multimodal_evaluate_api.py
@@ -9,9 +9,9 @@ import json
 from azure.identity import DefaultAzureCredential
 from azure.ai.evaluation import evaluate, ContentSafetyMultimodalEvaluator, ProtectedMaterialMultimodalEvaluator
 
-os.environ["AZURE_SUBSCRIPTION_ID"] = "b17253fa-f327-42d6-9686-f3e553e24763"
-os.environ["RESOURCE_GROUP"] = "hanchi-test"
-os.environ["PROJECT_NAME"] = "hancwang-eus2-0339"
+os.environ["AZURE_SUBSCRIPTION_ID"] = ""
+os.environ["RESOURCE_GROUP"] = ""
+os.environ["PROJECT_NAME"] = ""
 
 if __name__ == '__main__':
     
